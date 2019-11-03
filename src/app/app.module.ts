6c888976b4e98fd26250125cb06fe9eac6243eb7
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-03 13:14:19 +0300

    delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SearchComponent } from './search/search.component';
 import { UsersComponent } from './users/users.component';
 import { CompressPipe } from './compress.pipe';
 
@@ -14,13 +14,13 @@ import { CompressPipe } from './compress.pipe';
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    SearchComponent,
     UsersComponent,
     CompressPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
