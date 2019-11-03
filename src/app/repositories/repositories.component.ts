feb5d16c571576d3b03ce50b14a226289c9b1c50
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-03 21:58:27 +0300

    add code for collecting and emitting data

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../user.service'
+import { HomeComponent} from '../home/home.component'
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+  user: any
+  repos: any
+  username: any
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  searchUser(user){
+    this.getUserDetails(user)
+
+  }
+
+  getUserDetails(repo){
+    this.userService.getGithubUser(repo).then((response)=> {
+      this.user = response
+    })
+    this.userService.getGithubRepos(repo).then((response=>{
+      this.repos = response
+    }))
+
   }
 
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../'
+import { UserService } from '../user.service'
 
 @Component({
   selector: 'app-users',
@@ -7,10 +7,27 @@ import { UserService } from '../'
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user: any
+  repos: any
+  username: ''
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+  searchUser(user){
+    console.log(user)
+    this.getUserDetails(user)
+  }
+
+  getUserDetails(user){
+    this.userService.getGithubUser(user).then((response)=>{
+      this.user = response
+    })
+    this.userService.getGithubRepos(user).then((response)=>{
+      console.log(response)
+      this.repos = response
+    })
   }
 
 }
