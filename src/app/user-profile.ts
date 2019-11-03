c69165765dee1189a3c5eebd1f1cf21e6754deef
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-03 20:59:17 +0300

    add a new class

---
+++ src/app/user-profile.ts
@@ -0,0 +1,17 @@
+export class UserProfile {
+    constructor(
+        public avatarimg: string,
+        public user: string,
+        public owner: string,
+        public email: string,
+        public bio: string,
+        public followers: number,
+        public following: number,
+        public noOfrepos: number,
+        public gistNo: number,
+        public date: any
+    )
+    {
+        
+    }
+}
