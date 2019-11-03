e59169645544a8e64b008bf5106cacff750338d3
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-03 20:57:18 +0300

    add a new class

---
+++ src/app/repo-profile.spec.ts
@@ -0,0 +1,7 @@
+import { RepoProfile } from './repo-profile';
+
+describe('RepoProfile', () => {
+  it('should create an instance', () => {
+    expect(new RepoProfile()).toBeTruthy();
+  });
+});
---
+++ src/app/repo-profile.ts
@@ -0,0 +1,3 @@
+export class RepoProfile {
+    constructor(public myRepoArray){}
+}
